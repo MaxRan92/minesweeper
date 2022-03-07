e81edd9c93c5e4907ce35ad1669ff8550acd04b8
Massimo Ranalli <ranallimax@gmail.com>
2022-03-07 15:31:19 +0000

    Board size and bomb number variables assignment, create empty board

--- run.py
+++ run.py
@@ -1,7 +1,24 @@
-
-class create_board:
+class CreateBoard:
+    """
+    Create the gaming board
+    """
     def __init__(self, board_size, bomb_num):
-
+        """
+        board size and bomb number variable assignemnt 
+        """
         self.board_size = board_size
+        self.bomb_num = bomb_num
+
+        self.board = self.create_new_board()
+
+    def create_new_board(self):
+        """
+        Creat board arrays with none values
+        """
+        board = [[None for a in range(self.board_size)] for b in range(self.board_size)]
+        
+        print(board)
+
+CreateBoard(10, 10)
 
 
