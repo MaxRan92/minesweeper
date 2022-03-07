a4942296d4be4c7bf74fbc96dc26a5c67c989747
Massimo Ranalli <ranallimax@gmail.com>
2022-03-07 15:45:52 +0000

    Create while loop to place bomb at random coordinates

--- run.py
+++ run.py
@@ -1,10 +1,12 @@
+import random
+
 class CreateBoard:
     """
     Create the gaming board
     """
     def __init__(self, board_size, bomb_num):
         """
-        board size and bomb number variable assignemnt 
+        Board size and bomb number variable assignment 
         """
         self.board_size = board_size
         self.bomb_num = bomb_num
@@ -13,11 +15,27 @@ class CreateBoard:
 
     def create_new_board(self):
         """
-        Creat board arrays with none values
+        Create board arrays with none values
+        Place bombs, represented by character "*", at random coordinates in the board 
         """
         board = [[None for a in range(self.board_size)] for b in range(self.board_size)]
         
+        bomb_counter = 0
+        while bomb_counter < self.bomb_num:
+            # Generate two random coordinates considering the size of the board
+            x, y = random.randint(0, self.board_size - 1), random.randint(0, self.board_size - 1)
+            # If the cell of the board has already a bomb, continue (skip and go at the top of the while loop)
+            if board[x][y] == "*":
+                continue
+            # If the cell has no bomb, place it
+            board[x][y] = "*"
+            bomb_counter += 1
+        
         print(board)
+
+
+        
+
 
 CreateBoard(10, 10)
 
