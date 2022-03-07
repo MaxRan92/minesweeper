6ac4a00b3548aa966b378147c0ef3e9ba112189f
Massimo Ranalli <ranallimax@gmail.com>
2022-03-07 20:50:20 +0000

    Refactor initialization and add inser value function to count adjacent bomb

--- run.py
+++ run.py
@@ -4,14 +4,23 @@ class CreateBoard:
     """
     Create the gaming board
     """
-    def __init__(self, board_size, bomb_num):
+    def __init__(self):
         """
         Board size and bomb number variable assignment 
         """
-        self.board_size = board_size
-        self.bomb_num = bomb_num
-
+        self.board_size = 0
+        self.bomb_num = 0
+        self.get_difficulty_level()
         self.board = self.create_new_board()
+        self.insert_values()
+
+    def get_difficulty_level(self):
+        """
+        Assign board size and bomb num according to
+        the difficulty level chosen by the user
+        """
+        self.board_size = 20
+        self.bomb_num = 20
 
     def create_new_board(self):
         """
@@ -30,15 +39,47 @@ class CreateBoard:
             # If the cell has no bomb, place it
             board[x][y] = "*"
             bomb_counter += 1
+        return board
         
-        print(board)
-
-
     
-
-
+    def insert_values(self):
+        """
+        for each cell that has no bomb, assign a value representing
+        the number of bombs in the cells next to it
+        """
+        for x in range(self.board_size):
+            for y in range(self.board_size):
+                if self.board[x][y] == "*":
+                    continue
+                self.board[x][y] = self.get_near_bombs_num(x,y)
+        
         
 
-CreateBoard(20, 20)
+    def get_near_bombs_num(self,x,y):
+        """
+        #iterate through the 8 adiacent cells 
+        """        
+        near_bombs_num = 0
+        for r in range(max(0, x-1), min(self.board_size-1, x+1)+1):
+            for c in range(max(0, y-1), min(self.board_size-1, y+1)+1):
+                if r == x and c == y:
+                    continue
+                if self.board[r][c] == '*':
+                    near_bombs_num += 1
+        return near_bombs_num
 
+    def display_board():
+        """
+        display board with format
+        """
+
+
+def main():
+    #insert if you want to play or not
+    game = CreateBoard()
+    print(game.board)
+
+
+
+main()
 
