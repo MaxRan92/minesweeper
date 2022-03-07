a86415b9c240b8db0531db9f1dcbf40b22b849c0
Massimo Ranalli <ranallimax@gmail.com>
2022-03-07 15:14:19 +0000

    Add CreateBoard class

--- run.py
+++ run.py
@@ -1,3 +1,7 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+class create_board:
+    def __init__(self, board_size, bomb_num):
+
+        self.board_size = board_size
+
+
